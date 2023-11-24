2aa7c099bc8712c9a7f52c38fa42f371e7e222a8
NAJD <eastroqq@gmail.com>
2023-11-24 14:05:13 -0300

    Actually did optimized it lol

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 bool isPrime(int n) {
     // i hate my life
-    for (int i = 2; i <= n-1; i++) {
+    if (n <= 1) {
+       return false;
+    }
+    if (n == 2) {
+       return true;
+    }
+    if (n % 2 == 0) {
+       return false;
+    }
+    for (int i = 2; i <= sqrt(n); i++) {
         if (n % i == 0) {
             return false;
         }
